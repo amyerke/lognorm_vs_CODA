95565c1ef03a75b0b64a3d943331251b4e4300e4
Aaron_Y <amyerke@uncc.edu>
2023-07-11 17:57:35 -0400

    add filtering step for all transformations

--- lib/cml_scripts/transformations/make_nonilr_datasets.R
+++ lib/cml_scripts/transformations/make_nonilr_datasets.R
@@ -1,6 +1,9 @@
 # Author: Aaron Yerke (aaronyerke@gmail.com)
-# Script for making lognorm and coda tables to run through my random forest
-# Requires raw data2 output
+# Script for making noncoda and coda tables to run through my random forest
+# Requires raw data2 output. It also makes a 90% filtered asv table and makes
+# the same transformations using this table.
+# I.e. #creating an asv dataset where asvs that are in less than 10% of the
+# samples are removed
 
 rm(list = ls()) #clear workspace
 
@@ -37,12 +40,13 @@ option_list <- list(
                         help="dataset dir path", metavar="character"),
   optparse::make_option(c("-p", "--project"), type="character", default="Vangay", 
                         help="project folder", metavar="character"),
-  optparse::make_option(c("-m", "--metadata"), type="character", default=NULL,
-                        help="metadata file path with filename", metavar="character"),
-  optparse::make_option(c("-l", "--metadata_delim"), type="character", default="\t",
-                        help="metadata file deliminator", metavar="character"),
-  optparse::make_option(c("-r", "--metadata_rowname"), type="character", default=NULL,
-                        help="metadata file row to use for row names", metavar="character")
+  optparse::make_option(c("-i", "--initial_table"), type="character", 
+                        default="/r_objects/ForwardReads_DADA2.rds",
+                        help="initial table, relative to project path, with filename, 
+                        should be an r object"),
+  optparse::make_option(c("-r", "--output_file_prefix"), type="character", 
+                        default="",
+                        help="prefix to identify different types of output files"),
 ); 
 
 opt_parser <- optparse::OptionParser(option_list=option_list);
@@ -111,35 +115,55 @@ df <- vegan::rrarefy(low_abund_removed, rd)
 saveRDS(df, file = file.path(output_dir,"r_objects", paste0(rd,"_rrarefy_asv.rds")))
 write.csv(df, file = file.path(output_dir,"tables", paste0(rd,"_rrarefy_asv.csv")))
 
-# print("Creating hashseq lognorm, ALR and CLR.")
-# hashseq <- data.frame(data.table::fread(file = file.path(output_dir,"hashseq", "hashseq.csv"),
-#                                         header="auto", data.table=FALSE), row.names = 1)
-# if (dir.exists(file.path(output_dir,"r_objects", "lognorm_hashseq.rds"))) {
-#   ln_hs_tab <- readRDS(file.path(output_dir,"r_objects", "lognorm_HashSeq.rds"))
-# }else{
-# 	ln_hs_tab <- lognorm(hashseq)
-#   saveRDS(ln_hs_tab, file = file.path(output_dir,"r_objects", "lognorm_hashseq.rds"))
-#   write.csv(ln_hs_tab, file = file.path(output_dir,"tables", "lognorm_hashseq.csv"))
-# }
-# print("Making HashSeq clr.")
-# if (dir.exists(file.path(output_dir,"r_objects", "r_objects", "clr_hashseq.rds"))) {
-#   HashSeq_clr <- readRDS(file.path(output_dir,"r_objects", "clr_hashseq.rds"))
-# }else{
-#   HashSeq_clr <- as.data.frame(rgr::clr(as.matrix(hashseq + 1)))
-#   saveRDS(ln_hs_tab, file = file.path(output_dir,"r_objects", "clr_hashseq.rds"))
-#   write.csv(ln_hs_tab, file = file.path(output_dir,"tables", "clr_hashseq.csv"))
-# }
-# print("Making HashSeq alr.")
-# my_zeros <- apply(hashseq, 2, function(x) {
-#   return(sum(x == 0))
-# })
-# alr_col <- which(my_zeros == min(my_zeros))[1]
-# if (dir.exists(file.path(output_dir,"r_objects", "r_objects", "alr_hashseq.rds"))) {
-#   HashSeq_alr <- readRDS(file.path(output_dir,"r_objects", "alr_hashseq.rds"))
-# }else{
-#   HashSeq_alr <- as.data.frame(rgr::alr(as.matrix(hashseq + 1), j = as.numeric(alr_col)))
-#   saveRDS(HashSeq_alr, file = file.path(output_dir,"r_objects", "alr_hashseq.rds"))
-#   write.csv(HashSeq_alr, file = file.path(output_dir,"tables", "alr_hashseq.csv"))
-# }
+#### Creating the filtered dataset and repeating all transformations ####
+initial_table <- initial_table[sapply(initial_table, function(x) mean(x == 0) <= 0.9)]
+
+print("creating filtered DADA2 lognorm, ALR and CLR")
+if (!dir.exists(file.path(output_dir,"r_objects", "lognorm_asv.rds"))) {
+  df <- lognorm(initial_table)
+  saveRDS(df, file = file.path(output_dir,"r_objects", "filtered_90prcnt_lognorm_asv.rds"))
+  write.csv(df, file = file.path(output_dir,"tables", "filtered_90prcnt_lognorm_dada2.csv"))
+}
+#TODO: Add filtered silva lognorm
+# # if (!dir.exists(file.path(output_dir,"r_objects", "silva_lognorm.rds"))) {
+# silva_ps_robj <- readRDS(file.path(output_dir, "r_objects","ref_tree_phyloseq_obj.rds"))
+# df <- na.omit(silva_ps_robj@otu_table) 
+# df <- na.omit(lognorm(df))
+# saveRDS(df, file = file.path(output_dir,"r_objects", "lognorm_Silva.rds"))
+# write.csv(df, file = file.path(output_dir,"tables", "lognorm_Silva.csv"))
+# # }
+my_zeros <- apply(initial_table, 2, function(x) {
+  return(sum(x == 0))
+})
+alr_col <- which(my_zeros == min(my_zeros))[1]
+# alr_col_num <- grep(alr_col, colnames(initial_table))
+print("creating DADA2 ALR")
+df <- as.data.frame(rgr::alr(as.matrix(initial_table + 1), j = as.numeric(alr_col)))
+saveRDS(df, file = file.path(output_dir,"r_objects", "filtered_90prcnt_alr_asv.rds"))
+write.csv(df, file = file.path(output_dir,"tables", "filtered_90prcnt_alr_asv.csv"))
+
+print("creating DADA2 CLR")
+df <- as.data.frame(rgr::clr(as.matrix(initial_table + 1)))
+saveRDS(df, file = file.path(output_dir,"r_objects", "filtered_90prcnt_clr_asv.rds"))
+write.csv(df, file = file.path(output_dir,"tables", "filtered_90prcnt_clr_asv.csv"))
+
+print("creating DADA2 proportions")
+df <- proportions_transform(initial_table)
+saveRDS(df, file = file.path(output_dir,"r_objects", "filtered_90prcnt_propotions_asv.rds"))
+write.csv(df, file = file.path(output_dir,"tables", "filtered_90prcnt_propotions_asv.csv"))
+
+print("creating DADA2 heilinger")
+df <- heilinger_transform(initial_table)
+saveRDS(df, file = file.path(output_dir,"r_objects", "filtered_90prcnt_heilinger_asv.rds"))
+write.csv(df, file = file.path(output_dir,"tables", "filtered_90prcnt_heilinger_asv.csv"))
+
+print("Creating rarefied dataset at 1000 read depth")
+filtered_prefix <- "filtered_90prcnt_"
+rd <- 1000
+low_abund_removed <- filt_seq_dpth(rd, initial_table)
+df <- vegan::rrarefy(low_abund_removed, rd)
+saveRDS(df, file = file.path(output_dir,"r_objects", paste0(filtered_prefix,rd,"_rrarefy_asv.rds")))
+write.csv(df, file = file.path(output_dir,"tables", paste0(filtered_prefix,rd,"_rrarefy_asv.csv")))
 
 print("Reached end of script.")
+

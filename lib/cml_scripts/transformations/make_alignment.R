6e23d3c9cb247a79038f8a3382a155a418326786
Aaron_Y <amyerke@uncc.edu>
2023-07-30 21:41:56 -0400

    add DNA string names to alignment

--- lib/cml_scripts/transformations/make_alignment.R
+++ lib/cml_scripts/transformations/make_alignment.R
@@ -50,7 +50,7 @@ asv_table <- readRDS(file.path(output_dir, "r_objects", opt$input_table))
 print("Finished establishing directory layout")
 
 alignment <- DECIPHER::AlignSeqs(DNAStringSet(colnames(asv_table)), anchor=NA,verbose=FALSE)
-
+names(alignment) <- 1:length(alignment)
 writeXStringSet(alignment, file = file.path(output_dir, "tables", paste0(opt$output_file, ".fasta")), 
                 append=FALSE, compress=FALSE, compression_level=NA, format="fasta")
 

61a994b53587949ce6a1ff9b09e1c443aaccf964
Aaron_Y <amyerke@uncc.edu>
2023-09-05 22:23:40 -0400

    bug

--- metastudies/scripts/compare_rand_forest/summary_accuracy_ave_relative.py
+++ metastudies/scripts/compare_rand_forest/summary_accuracy_ave_relative.py
@@ -47,7 +47,7 @@ home_dir = os.path.expanduser(options.homedir)
 projects = ["Jones", "Vangay", "Zeller", "Noguera-Julian"]
 output_dir = os.path.join(home_dir, "metastudies", "output")
 assert os.path.exists(output_dir)
-plot_pdf_fpath = os.path.join(output_dir, "philr_summary_ave_acc_vs_acc_python_by_transformation.pdf")
+plot_pdf_fpath = os.path.join(output_dir, "relative_summary_ave_acc_vs_acc_python_by_transformation.pdf")
 # --------------------------------------------------------------------------
 print("Establishing other constants.", flush = True)
 # --------------------------------------------------------------------------

4bb98005328bdb8f25687d4fa9df40bd27aff4a7
Aaron_Y <amyerke@uncc.edu>
2023-10-01 15:46:00 -0400

    script for calculating mapped coverage of silva ref tree.

--- metastudies/scripts/data_aggregation/coverage_asv_vs_silva.R
+++ metastudies/scripts/data_aggregation/coverage_asv_vs_silva.R
@@ -1,10 +1,22 @@
 # Author: Aaron Yerke (aaronyerke@gmail.com)
-# Script for determining median read depth
+# Script for determining what proportion of ASV's went into
+# the Silva LTP
 
 rm(list = ls()) #clear workspace
 
 ####-Functions--------------------------------------------------------####
 print("Loading custom functions.")
+raw_ps_to_clean_ps <- function(ps) {
+  #requires ape, phyloseq and philr_tutorial_normalization 
+  #performs philr tutorial normalization on phyloseq obj
+  clean_otu = data.frame(ps@otu_table@.Data)
+  clean_otu = philr_tutorial_normalization(clean_otu)
+  ps_clean = phyloseq::phyloseq( otu_table(clean_otu, taxa_are_rows = F), 
+                                 phy_tree(ps@phy_tree),
+                                 tax_table(ps@tax_table), 
+                                 sample_data(ps@sam_data))
+  return(ps_clean)
+}
 
 ####-Load Depencencies------------------------------------------------####
 options(repos = list(CRAN="http://cran.rstudio.com/"))
@@ -46,11 +58,34 @@ projects <- c("Jones", "Vangay", "Zeller", "Noguera-Julian")
 source(file.path(home_dir, "lib", "table_manipulations.R"))
 
 #### Importing and preprocessing tables, starting with DADA2 ####
-for (project in projects) {
-  output_dir <- file.path(home_dir, project, 'output')
+print("Importing and preprocessing tables, starting with DADA2")
+
+output_df <- data.frame("project" = character(length = 4),
+                        "raw_counts" = numeric(length = 4),
+                        "raw_counts_SilvaLTP" = numeric(length = 4),
+                        "raw_proportion_mapped" = numeric(length = 4),
+                        "prev_filt_counts" = numeric(length = 4),
+                        "prev_filt_SilvaLTP" = numeric(length = 4),
+                        "pf_proportion_mapped" = numeric(length = 4)
+                        )
+
+print(output_df)
+
+for (pro in 1:length(projects)) {
+	print(pro)
+  project <- projects[pro]
+	print(project)
+  output_df$project[pro] <- project
+  output_dir <- file.path(home_dir, project, "output")
   initial_table <- readRDS(file.path(output_dir, "r_objects", "ForwardReads_DADA2.rds"))
-  read_depth <- rowSums(initial_table)
-  print(project)
-  print(paste(project, "median read depth:", median(read_depth)))
-  fraction mapped <- 
+  output_df$raw_counts[pro] <- ncol(initial_table)
+  silva <- readRDS(file.path(output_dir, "r_objects", "ref_tree_phyloseq_obj.rds"))
+  output_df$raw_counts_SilvaLTP[pro] <- ncol(silva@otu_table)
+  output_df$raw_proportion_mapped[pro] <- ncol(silva@otu_table) / ncol(initial_table)
+
+  initial_table <- readRDS(file.path(output_dir, "r_objects", "filtered_90prcnt_dada2.rds"))
+  output_df$prev_filt_counts[pro] <- ncol(initial_table)
+  silva <- readRDS(file.path(output_dir, "r_objects", "ltp_90prcnt_filt.rds"))
+  output_df$prev_filt_SilvaLTP[pro] <- ncol(silva@otu_table)
+  output_df$pf_proportion_mapped[pro] <- ncol(silva@otu_table) / ncol(initial_table)
 }
